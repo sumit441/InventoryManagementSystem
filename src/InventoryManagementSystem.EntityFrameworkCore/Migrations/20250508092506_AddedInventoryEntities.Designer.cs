43e6f1223e4c534c516ee08da9174ecf5ff29a5c
Sales App <xumitlama23@gmail.com>
2025-05-08 15:14:28 +0545

    Add project files.

--- src/InventoryManagementSystem.EntityFrameworkCore/Migrations/20250508092506_AddedInventoryEntities.Designer.cs
+++ src/InventoryManagementSystem.EntityFrameworkCore/Migrations/20250508092506_AddedInventoryEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InventoryManagementSystem.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace InventoryManagementSystem.Migrations
 {
     [DbContext(typeof(InventoryManagementSystemDbContext))]
-    partial class InventoryManagementSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508092506_AddedInventoryEntities")]
+    partial class AddedInventoryEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
